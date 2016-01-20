03f337662841464223c5ef47e211c57807b7bbb3
Stuart R. Jefferys <srjefferys@gmail.com>
2016-01-20 16:17:16 -0500

    Doc: Add readme.rmd, update description slightly.
    Template readme.rmd added with devtools::use_readme_rmd()  which includes git hook to prevent committing if don't rebuild the readme.md file

--- R/DataRepo.R
+++ R/DataRepo.R
@@ -2,10 +2,11 @@
 #'
 #' This package helps manage a large (100+ TB) collection of data files. It only
 #' works on a unix-like file system (i.e. not on Windows) as it uses unix file
-#' system groups and permissions to manage access. Access is not very granular,
-#' although read permission is separate from write/delete permission. This is
-#' currently in a very preliminary state and under active development. Don't
-#' count on API consistency across versions yet.
+#' system groups and permissions to manage data access. Access is not very
+#' granular, with read permission granted by membership in a file system group,
+#' the "read group" and write permission granted by membership in another group,
+#' the "write-group". This is currently in a very preliminary state and under
+#' active development. Don't count on API consistency across versions yet.
 #'
 #' See the [SRJ-TODO] vignette for an overview of how to use this package.
 #'
---
+++ README.md
@@ -0,0 +1,7 @@
+<!-- README.md is generated from README.Rmd. Please edit that file -->
+DataRepo
+========
+
+The DataRepo package helps manage a large (100+ TB) collection of data files. It only works on a unix-like file system (i.e. not on Windows) as it uses unix file system groups and permissions to manage data access. Access is not very granular, with read permission granted by membership in a file system group, the "read group" and write permission granted by membership in another group, the "write-group".
+
+This is currently in a very preliminary state and under active development. Don't count on API consistency across versions yet.
